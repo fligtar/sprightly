e897d875ed4ad7fe9b36a7da213503630faf1c1b
Justin Scott <fligtar@gmail.com>
2010-05-18 03:04:12 -0700

    first pass at redoing the amo feature module

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ $office = !empty($_GET['office']) && in_array($_GET['office'], $offices) ? $_GET
         <section id="middle">
             
             <div id="rotating">
-                <span class="featured">Featured Projects<span class="next">next: <span class="label">Support</span> in <time class="relative mins_until"></time>m</span></span>
+                <span class="featured">Featured Projects<!--><span class="next">next: <span class="label">Support</span> in <time class="relative mins_until"></time>m</span>--></span>
                 
                 <div id="amo" class="box active" data-label="Add-ons" data-duration="15">
                     
@@ -130,13 +130,20 @@ $office = !empty($_GET['office']) && in_array($_GET['office'], $offices) ? $_GET
                     
                     <!-- General Stats. inline display:block is required to stop a warping bug. don't ask -->
                     <div id="amo-1" class="panel active" style="display: block;">
-                        <div>
-                            <span id="amo-public" class="count"></span>
-                            <span class="label">approved add-ons and Personas</span>
+                        <div class="stats-container">
+                                <div>
+                                    <span id="amo-public" class="count"></span>
+                                    <span class="label">approved add-ons and Personas</span>
+                                </div>
+                                
+                                <div>
+                                    <span id="amo-collections" class="count"></span>
+                                    <span class="label">collections created</span>
+                                </div>
                         </div>
-                        <div>
+                        <div class="stats-container">
                             <div>
-                                <span id="amo-downloads" class="count"></span>
+                                <span id="amo-downloads-temp" class="count">1,947,652,054</span>
                                 <span class="label">add-on downloads</span>
                             </div>
 
@@ -145,51 +152,89 @@ $office = !empty($_GET['office']) && in_array($_GET['office'], $offices) ? $_GET
                                 <span class="label">add-ons in use</span>
                             </div>
                         </div>
-                    </div>
-                    
-                    <!-- Collections -->
-                    <div id="amo-2" class="panel">
-                    <p>Since their launch last June, <strong><span id="amo-collections"></span> collections have been created</strong> by users, generating <strong><span id="amo-collectiondownloads"></span> add-on downloads</strong> from those collections.</p>
+                        
+                        <p class="caption"><em>(imagine this text is a chart)</em></p>
                     </div>
                     
                     <!-- Review Queues -->
+                    <div id="amo-2" class="panel">
+                        <p class="intro">Our review queues have been consistently under control since Firefox 3.6.</p>
+                        
+                        <div class="stats-container">
+                                <div>
+                                    <span id="amo-pending" class="count"></span>
+                                    <span class="label">pending updates to public add-ons</span>
+                                </div>
+                                
+                                <div>
+                                    <span id="amo-nominated" class="count"></span>
+                                    <span class="label">new add-ons awaiting review</span>
+                                </div>
+                        </div>
+                        
+                        <div class="stats-container">
+                                <div>
+                                    <span class="count">90%</span>
+                                    <span class="label">of pending updates reviewed in</span>
+                                    <span class="count">under 5 days</span>
+                                </div>
+                                
+                                <div>
+                                    <span class="count">97%</span>
+                                    <span class="label">of new add-ons reviewed in</span>
+                                    <span class="count">under 2 weeks</span>
+                                </div>
+                        </div>
+                        
+                        <p>Thanks to our editor team of volunteers, contractors, and employees!</p>
+                    </div>
+                    
+                    <!-- What's New -->
                     <div id="amo-3" class="panel">
-                    <p>Major progress has been made on the review queues, and there are currently <strong><span id="amo-pending"></span> pending updates</strong> to add-ons and <strong><span id="amo-nominated"></span> new add-ons</strong> awaiting Editor review.</p>
-                    </div>
-                    
-                    <!-- RYF -->
-                    <div id="amo-4" class="panel">
-                        <p>Rock Your Firefox, our new blog featuring 3 consumer-friendly add-ons every week, continues to entertain its <strong><span>5,000+</span> daily readers</strong> and highlight some of the best ways to customize Firefox.</p>
-                    </div>
-                    
-                    <!-- Roadmap -->
-                    <div id="amo-5" class="panel">
-                        <p>What the add-ons team is currently working on:</p>
-                        <ul>
-                            <li>rewriting addons.mozilla.org in Django</li>
-                            <li>absorbing GetPersonas.com</li>
-                            <li>helping with the Firefox 4 Add-ons Manager redesign</li>
-                            <li>working on big improvements to core features and tools</li>
-                            <li>planning developer and consumer events</li>
-                            <li>Jetpack, marketplace, and lots more</li>
+                        <h2>Recent News</h2>
+                        <ul class="spaced">
+                            <li>Our first Zamboni pages (Django rewrite) went live in early May and are now serving 100% of homepage and add-on details page traffic</li>
+                            <li>We're holding a Mozilla Add-ons Workshop in London on June 30</li>
+                            <li>Rock Your Firefox continues to feature several consumer-friendly add-ons each week with great commentary</li>
+                            
                         </ul>
                     </div>
                     
+                    <!-- Coming Soon -->
+                    <div id="amo-4" class="panel">
+                        <h2>Coming Up</h2>
+                        <ul class="spaced">
+                            <li>New AMO interactions in the Firefox 4 Add-ons Manager</li>
+                            <li>Aiming to launch all public pages in Django rewrite by end of Q2</li>
+                            <li>Major revamps of Developer Tools and our review process coming in Q3</li>
+                            <li>Add-ons Marketplace discussion at the Summit</li>
+                        </ul>
+                    </div>
+                    
+                    <!-- Get Involved -->
+                    <div id="amo-5" class="panel">
+                        <h2>Non-technical Ways to Help </h2>
+                        <p>Get involved with AMO by writing reviews of add-ons you use, creating collections, testing the site, and filing bugs and enhancement requests.</p>
+                        
+                        <h2>Technical Ways to Help</h2>
+                        <p>Becoming an editor and reviewing add-ons is a great way to see how people are using the platform and what problems they encounter. Answering questions on forums.addons.mozilla.org will unblock a stuck developer.</p>
+                        <p>And please, if you write an add-on, host it on AMO and not your people.mozilla.com account! It's safer, more discoverable, promotes AMO, and provides stats.</p>
+                    </div>
+                    
                     <ul class="menu">
-                        <li id="amo-1m" class="active">Overview</li>
-                        <li id="amo-2m">Collections</li>
-                        <li id="amo-3m">Review Queues</li>
-                        <li id="amo-4m">Rock Your Firefox</li>
-                        <li id="amo-5m">Roadmap</li>
+                        <li id="amo-1m" class="active">Statistics</li>
+                        <li id="amo-2m">Review Queues</li>
+                        <li id="amo-3m">What's New</li>
+                        <li id="amo-4m">Coming Soon</li>
+                        <li id="amo-5m">Get Involved</li>
                     </ul>
                     
                 </div><!-- /#amo -->
                 
-                <div id="sumo" class="box" data-label="Support" data-duration="2">
+                <!--><div id="sumo" class="box" data-label="Support" data-duration="2">
 
                     <h1><img src="images/sumo.png"/><span>Support</span></h1>
 
-                    <!-- Test 1 -->
                     <div id="support-1" class="panel active">
                         <p>Cheng, give me support data!</p>
                     </div>
@@ -198,7 +243,7 @@ $office = !empty($_GET['office']) && in_array($_GET['office'], $offices) ? $_GET
                         <li id="support-1m" class="active">A Plea</li>
                     </ul>
 
-                </div><!-- /#support -->
+                </div>-->
                 
             </div><!-- /#rotating -->
             
